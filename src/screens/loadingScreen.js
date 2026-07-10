d06d20d70d3201d2d27d26b53bf55490a1816ee0
Nguyen Danh <ngbdanh239@gmail.com>
2026-07-10 17:13:01 +0700

    feat: implement progress bar for LoadingScreen

--- src/screens/loadingScreen.js
+++ src/screens/loadingScreen.js
@@ -50,10 +50,37 @@ export default class LoadingScreen extends BaseScreen {
     }
 
     drawProgressBar() {
+        const barWidth = 600;
+        const barHeight = 40;
+        const xPos = (GAME_CONFIG.SCREEN_WIDTH - barWidth) / 2;
+        const yPos = 1000;
 
+        const barOutline = new Graphics();
+        barOutline
+            .roundRect(xPos, yPos, barWidth, barHeight, 20)
+            .fill(0x555555)
+            .stroke({ width: 4, color: 0x333333 });
+
+        this.progressBarFill = new Graphics();
+        this.progressBarFill
+            .roundRect(xPos, yPos, 0, barHeight, 20)
+            .fill(0xffd700);
+
+        this.container.addChild(barOutline);
+        this.container.addChild(this.progressBarFill);
     }
 
     updateProgress(percent) {
+        const clampedPercent = Math.max(0, Math.min(1, percent));
 
+        const barWidth = 600;
+        const barHeight = 40;
+        const xPos = (GAME_CONFIG.SCREEN_WIDTH - barWidth) / 2;
+        const yPos = 1000;
+
+        this.progressBarFill.clear();
+        this.progressBarFill
+            .roundRect(xPos, yPos, barWidth * clampedPercent, barHeight, 20)
+            .fill(0xFFD700);
     }
 }
