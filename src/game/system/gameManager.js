a5e0514c41b5dd58877aa4b0fb61910d637d6d93
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-10 11:03:40 +0700

    feat: initialize the constructor for game manager

--- src/game/system/gameManager.js
+++ src/game/system/gameManager.js
@@ -1,5 +1,17 @@
 class GameManager{
-    constructor(){}
+    constructor(app){
+        this.app = app;
+
+        this.isGameOver = false;
+        this.isGameRunning = false;
+        this.isGamePause = false;
+
+        this.score = 0;
+
+        this.currentAnimal = null;
+        this.nextAnimal = null;
+        this.animalPool = [];
+    }
 
     start(){}
 
