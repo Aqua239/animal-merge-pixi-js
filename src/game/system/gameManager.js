08e4697f6e1de2ea968b637c680eaffd0d8e6edd
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-10 12:28:38 +0700

    feat: add current and next animal spawning

--- src/game/system/gameManager.js
+++ src/game/system/gameManager.js
@@ -1,3 +1,5 @@
+import { Animal } from "../entities/animal";
+
 export class GameManager{
     constructor(app){
         this.app = app;
@@ -7,6 +9,8 @@ export class GameManager{
         this.isGamePause = false;
 
         this.score = 0;
+        this.isSpawner = false;
+        this.isDrop = false;
 
         this.currentAnimal = null;
         this.nextAnimal = null;
@@ -20,6 +24,8 @@ export class GameManager{
         this.isGameOver = false;
 
         this.app.ticker.add(this.update.bind(this));
+
+        this.initSpawn(100,50,50,50);
     }
 
     pause(){
@@ -48,5 +54,47 @@ export class GameManager{
         this.animalPool = [];
     }
 
-    update(){}
+    update(ticker){
+        if(!this.isGameRunning || this.isGameOver || this.isGamePause) return;
+    }
+
+    initSpawn(xSpawnNext, ySpawnNext, xSpawnCurrent, ySpawnCurrent){
+        if(this.nextAnimal === null){
+            this.isSpawner = true;
+            let randomLevel = Math.floor(Math.random() * 3) + 1;
+            this.nextAnimal = this.spawner(xSpawnNext, ySpawnNext, randomLevel);
+        }
+
+        if(this.currentAnimal === null){
+            this.isSpawner = true;
+            let randomLevel = Math.floor(Math.random() * 3) + 1;
+            this.currentAnimal = this.spawner(xSpawnCurrent, ySpawnCurrent, randomLevel);
+            this.isDrop = true;
+        }
+    }
+
+    stateAnimalForScene(xSpawn, ySpawn){
+        if(this.currentAnimal === null){
+            this.currentAnimal = this.nextAnimal;
+            if(this.currentAnimal){
+                this.currentAnimal.x = 50;
+                this.currentAnimal.y = 50;
+                this.isDrop = true;
+            }
+        }
+
+        if(this.nextAnimal === null || this.nextAnimal === this.currentAnimal){
+            this.isSpawner = true;
+            let randomLevel = Math.floor(Math.random() * 3) + 1;
+            this.nextAnimal = this.spawner(xSpawn, ySpawn, randomLevel);
+        }
+    }
+
+    spawnAnimal(xSpawn, ySpawn, level){
+        if(!this.isSpawner) return;
+        this.isSpawner = false;
+        let newAnimal = new Animal(level, xSpawn, ySpawn);
+        this.app.stage.addChild(newAnimal);
+        return newAnimal;
+    }
 }
