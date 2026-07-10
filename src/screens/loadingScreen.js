144e4f7dce4fda00e046da03d2bd9e8eb0736b48
Nguyen Danh <ngbdanh239@gmail.com>
2026-07-10 16:38:43 +0700

    feat: implement game's name rendering for LoadingScreen

--- src/screens/loadingScreen.js
+++ src/screens/loadingScreen.js
@@ -21,14 +21,32 @@ export default class LoadingScreen extends BaseScreen {
             GAME_CONFIG.ORIGIN_X,
             GAME_CONFIG.ORIGIN_Y,
             GAME_CONFIG.SCREEN_WIDTH,
-            GAME_CONFIG.SCREEN_HEIGHT);
+            GAME_CONFIG.SCREEN_HEIGHT
+        );
         background.endFill();
 
         this.container.addChild(background);
     }
 
     drawGameName() {
-        
+        const style = {
+            fontFamily: "Arial",
+            fontSize: 100,
+            fill: 0x2c365a,
+            fontWeight: "bold",
+            dropShadow: false,
+            dropShadowColor: "#FFFFFF",
+            dropShadowBlur: 5,
+            dropShadowDistance: 5,
+        };
+
+        const title = new Text("Animal Merge", style);
+
+        title.anchor.set(0.5);
+        title.x = GAME_CONFIG.SCREEN_WIDTH / 2;
+        title.y = 700;
+
+        this.container.addChild(title);
     }
 
     drawProgressBar() {
