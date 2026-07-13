347079cb0de874cb02118c305b5192d0a1510051
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-13 16:07:45 +0700

    feat: initialize the constructor for removeItem

--- src/game/entities/items/removeItem.js
+++ src/game/entities/items/removeItem.js
@@ -1,7 +1,13 @@
 import { SupportItem } from "./supportItem";
 
 export class RemoveItem extends SupportItem{
-    constructor(){}
+    constructor({texture, quantity = 0, onUse = null, x = 0, y = 0, width = 10, height = 10}){
+        super({texture, quantity, onUse});
+        this.x = x;
+        this.y = y;
+        this.sprite.width = width;
+        this.sprite.height = height;
+    }
 
     use(){}
 }
