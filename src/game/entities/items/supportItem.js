c42758e8a997635e891b324b97df08961c64f6c5
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-13 15:20:37 +0700

    feat: initialize the constructor for supportItem

--- src/game/entities/items/supportItem.js
+++ src/game/entities/items/supportItem.js
@@ -1,7 +1,32 @@
-import { Container } from "pixi.js";
+import { Container, Sprite } from "pixi.js";
 
 class SupportItem extends Container{
-    constructor(){}
+    constructor({
+        texture,
+        quantity = 0,
+        onUse = null,
+    }){
+        super();
+        this.quantity = quantity;
+        this.onUse = onUse;
+        this.isActive = false;
+
+        this.sprite = Sprite.from(texture);
+        this.sprite.anchor.set(0.5);
+        this.addChild(this.sprite);
+
+        this.eventMode = "static";
+        this.cursor = "pointer";
+
+        this.on("pointerdown", (event) => {
+            event.stopPropagation();
+            if(!this.isActive){
+                this.activate();
+            }else{
+                this.deactive();
+            }
+        });
+    }
 
     activate(){}
 
