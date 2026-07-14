2f9cd3e83e839c7f05a52b233cf7737d1f5cf3e1
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-14 10:03:14 +0700

    feat: initialize the constructor for gameStore

--- src/game/Store/gameStore.js
+++ src/game/Store/gameStore.js
@@ -1,7 +1,19 @@
-class GameStore{
-    constructor(){}
+import { DEFAULT_GAME_DATA, STORAGE_KEY } from "./data";
 
-    load(){}
+class GameStore{
+    constructor(){
+        this.data = this.load();
+    }
+
+    load(){
+        const rawData = localStorage.getItem(STORAGE_KEY);
+        if(!rawData){
+            return structuredClone(DEFAULT_GAME_DATA);
+        }
+
+        const savedData = JSON.parse(rawData);
+        return this.mergeWithDefault(savedData);
+    }
 
     save(){}
 
