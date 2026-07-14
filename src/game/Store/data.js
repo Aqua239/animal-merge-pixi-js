8ffc0b0d07844fbbccdeb8b7c02996db072d3c86
Hồ Duy Lợi <nasinho01@gmail.com>
2026-07-14 09:32:59 +0700

    feat: initialize data save file

---
+++ src/game/Store/data.js
@@ -0,0 +1,17 @@
+export const STORAGE_KEY = "merge-animal-save";
+
+export const DEFAULT_GAME_DATA = {
+  version: 1,
+
+  player: {
+    coin: 0,
+  },
+
+  scores: {
+    top3: [],
+  },
+
+  items: {
+    removeAnimal: 0,
+  },
+};
