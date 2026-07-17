d698f60ec54edaaa3d378711a4875c7b9e94b8aa
Nguyen Danh <ngbdanh239@gmail.com>
2026-07-17 14:26:34 +0700

    feat: add merge VFX and SFX

--- src/game/gameManager.js
+++ src/game/gameManager.js
@@ -133,6 +133,9 @@ export class GameManager {
 
         for (let animal of this.animalPool) {
             animal.setSpriteFollowCollider();
+            if (animal.updateScale) {
+                animal.updateScale();
+            }
         }
     }
 
